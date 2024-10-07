f7456e8551ff22d571b72f6d86f4e9c11403eda9
LakshanShalintha <nlsperera@std.appsc.sab.ac.lk>
2024-10-07 22:43:08 +0530

    Create Langing Page

---
+++ frontend/lib/screens/Landing_Page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class Landing_Page extends StatelessWidget {
+  const Landing_Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Hi',
+        style: TextStyle(fontSize: 24), // You can customize the font size and style
+      ),
+    );
+  }
+}
