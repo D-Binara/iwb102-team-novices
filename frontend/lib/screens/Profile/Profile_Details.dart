ae837b66e846260290860b155e1a1ba344fc3049
D-Binara <dbkaveesha@gmail.com>
2024-10-20 20:50:35 +0530

    modify home page and edit profile details

--- frontend/lib/screens/Profile/Profile_Details.dart
+++ frontend/lib/screens/Profile/Profile_Details.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/components/NavBar.dart';
+import 'package:frontend/services/auth_provider.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:provider/provider.dart';
 
 class ProfileDetails extends StatefulWidget {
   const ProfileDetails({super.key});
@@ -18,7 +20,8 @@ class _ProfileDetailsState extends State<ProfileDetails> {
   // Function to pick an image from the gallery
   Future<void> _pickImage() async {
     final ImagePicker picker = ImagePicker();
-    final XFile? pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    final XFile? pickedFile =
+        await picker.pickImage(source: ImageSource.gallery);
 
     if (pickedFile != null) {
       setState(() {
@@ -86,21 +89,15 @@ class _ProfileDetailsState extends State<ProfileDetails> {
     });
   }
 
-  // Function to validate and update the form
-  void _validateAndSubmit() {
-    if (_formKey.currentState!.validate()) {
-      // If the form is valid, perform the update action
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Form is valid, updating...')),
-      );
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
+    // Accessing AuthProvider to get user details
+    final authProvider = Provider.of<AuthProvider>(context);
+
     return GestureDetector(
       onTap: () {
-        FocusScope.of(context).unfocus(); // Hide the keyboard when tapping outside
+        FocusScope.of(context)
+            .unfocus(); // Hide the keyboard when tapping outside
       },
       child: Scaffold(
         appBar: AppBar(
@@ -108,8 +105,10 @@ class _ProfileDetailsState extends State<ProfileDetails> {
           title: const Text("Profile Details"),
           elevation: 0,
         ),
-        resizeToAvoidBottomInset: true, // Allows the body to resize when keyboard shows
-        body: SingleChildScrollView( // Makes the page scrollable
+        resizeToAvoidBottomInset:
+            true, // Allows the body to resize when keyboard shows
+        body: SingleChildScrollView(
+          // Makes the page scrollable
           padding: const EdgeInsets.all(16.0),
           child: Form(
             key: _formKey, // Assign the form key to the form
@@ -119,38 +118,44 @@ class _ProfileDetailsState extends State<ProfileDetails> {
                 // Centering the Circle Avatar at the top
                 Center(
                   child: Stack(
-                    alignment: Alignment.bottomRight, // Align the icon at the bottom right
+                    alignment: Alignment
+                        .bottomRight, // Align the icon at the bottom right
                     children: [
                       Container(
                         decoration: BoxDecoration(
                           shape: BoxShape.circle,
-                          border: Border.all(color: Colors.green, width: 2), // Add border with width 2
+                          border: Border.all(
+                              color: Colors.green,
+                              width: 2), // Add border with width 2
                         ),
                         child: CircleAvatar(
                           radius: 80, // Circle size
                           backgroundColor: Colors.green[200], // Circle color
-                          backgroundImage: _image != null ? FileImage(_image!) : null, // Display selected image
+                          backgroundImage: _image != null
+                              ? FileImage(_image!)
+                              : null, // Display selected image
                           child: _image == null
                               ? const Text(
-                            'Profile', // Example text inside the circle
-                            style: TextStyle(color: Colors.white, fontSize: 30),
-                          )
-                              : null, // Remove text when an image is selected
+                                  'Profile',
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 30),
+                                )
+                              : null,
                         ),
                       ),
                       // Camera Icon Button
                       IconButton(
-                        padding: const EdgeInsets.all(5.0), // Padding around the icon
-                        onPressed: _pickImage, // Call image picker when pressed
+                        padding: const EdgeInsets.all(5.0),
+                        onPressed: _pickImage,
                         icon: Container(
                           decoration: const BoxDecoration(
                             shape: BoxShape.circle,
-                            color: Colors.transparent, // Background color for the icon
+                            color: Colors.transparent,
                           ),
                           child: const Icon(
-                            Icons.camera_alt, // Camera icon
-                            color: Colors.black, // Icon color
-                            size: 25, // Icon size
+                            Icons.camera_alt,
+                            color: Colors.black,
+                            size: 25,
                           ),
                         ),
                       ),
@@ -158,113 +163,48 @@ class _ProfileDetailsState extends State<ProfileDetails> {
                   ),
                 ),
 
-                const SizedBox(height: 20), // Space below the circle
+                const SizedBox(height: 20),
 
-                // Additional Information Text
                 const Text(
                   "Additional Information",
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
-                const SizedBox(height: 30), // Space below the title
-
-                // Username Field
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: 'Username',
-                    hintText: 'Enter your username',
-                    labelStyle: const TextStyle(color: Colors.grey),
-                    contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.black),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.green),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter the username'; // Error message
-                    }
-                    return null;
-                  },
+                const SizedBox(height: 30),
+                Text(
+                  'First Name: ${authProvider.firstname}',
+                  style: const TextStyle(fontSize: 18),
                 ),
-                const SizedBox(height: 20), // Space between fields
-
-                // Email Field
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    hintText: 'Enter your email address',
-                    labelStyle: const TextStyle(color: Colors.grey),
-                    contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.black),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.green),
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a valid email'; // Error message
-                    } else if (!RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$').hasMatch(value)) {
-                      return 'Please enter a valid email'; // Email validation error
-                    }
-                    return null;
-                  },
+                const SizedBox(height: 20),
+                Text(
+                  'Last Name: ${authProvider.firstname}',
+                  style: const TextStyle(fontSize: 18),
                 ),
-                const SizedBox(height: 20), // Space between fields
-
-                // Password Field with visibility toggle
-                TextFormField(
-                  obscureText: !_isPasswordVisible,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    hintText: 'Enter the password',
-                    labelStyle: const TextStyle(color: Colors.grey),
-                    contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.black),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.green),
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        _isPasswordVisible ? Icons.visibility_off : Icons.visibility_outlined,
-                        color: Colors.green,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          _isPasswordVisible = !_isPasswordVisible; // Toggle password visibility
-                        });
-                      },
-                    ),
-                  ),
-                  validator: (value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Please enter a password'; // Error message
-                    } else if (value.length < 6) {
-                      return 'Password must be at least 6 characters'; // Length validation error
-                    }
-                    return null;
-                  },
+                const SizedBox(height: 20),
+                Text(
+                  'Username: ${authProvider.firstname} ${authProvider.lastname}',
+                  style: const TextStyle(fontSize: 18),
                 ),
-                const SizedBox(height: 20), // Space below the last field
+                const SizedBox(height: 20),
+
+                Text(
+                  'Email: ${authProvider.email}',
+                  style: const TextStyle(fontSize: 18),
+                ),
+                const SizedBox(height: 20),
 
                 // Update Button
                 ElevatedButton(
-                  onPressed: _validateAndSubmit, // Call validation and submission
+                  onPressed: () {
+                    // This can be used for any update logic
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                          content: Text('Profile updated successfully!')),
+                    );
+                  }, // Call validation and submission
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.green, // Button color
-                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 100),
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 10, horizontal: 100),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(8), // Rounded corners
                     ),
@@ -282,10 +222,4 @@ class _ProfileDetailsState extends State<ProfileDetails> {
       ),
     );
   }
-}
-
-void main() {
-  runApp(const MaterialApp(
-    home: ProfileDetails(),
-  ));
 }
